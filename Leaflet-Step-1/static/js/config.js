ecd73890a8d8f1cd71c1948f8e0a345151453170
Sarah Heavner <sheavner@gmail.com>
2021-01-24 12:15:15 -0700

    set initial map

--- Leaflet-Step-1/static/js/config.js
+++ Leaflet-Step-1/static/js/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = "YOUR API KEY HERE!";
+const API_KEY = "pk.eyJ1Ijoic2FyYWhoZWF2bmVyIiwiYSI6ImNranp0Z2R2ejAyajIyd3F4eG44cndueTAifQ.01J6o7WFZea3mdn_hImk0g";
---
+++ Leaflet-Step-1/static/js/logic.js
@@ -0,0 +1,51 @@
+//set initial map coordinates
+var myMap = L.map("map", {
+  center: [37.09, -95.71],
+  zoom: 4
+});
+
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/streets-v11",
+    accessToken: API_KEY
+  }).addTo(myMap);
+
+
+
+// create variable to store API url
+queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+
+// Perform a GET request to the query URL
+d3.json(queryUrl, function(data) {
+
+    //log data.features in console to ensure API request functioning
+    console.log(data.features);
+
+    //send the data.features object to the createFeatures function
+    createFeatures(data.features);
+});
+
+
+function createFeatures(earthquakeData) {
+
+    //Define a function we want to run once for each feature in the features array
+    // Give each feature a popup describing the place and time of the earthquake
+    function onEachFeature(feature, layer) {
+        layer.bindPopup("<h3>" + feature.properties.place +
+          "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
+    }
+
+    // Create a GeoJSON layer containing the features array on the earthquakeData object
+    // Run the onEachFeature function once for each piece of data in the array
+    var earthquakes = L.geoJSON(earthquakeData, {
+        onEachFeature: onEachFeature
+    });
+
+    // Sending our earthquakes layer to the createMap function
+    // createMap(earthquakes);
+}
+
+
